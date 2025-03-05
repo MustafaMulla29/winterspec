4d30db60ec18f9965c0b50322c33a298d8a7629f
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-03-05 17:57:49 +0530

    Update create-with-winter-spec.ts

--- src/create-with-winter-spec.ts
+++ src/create-with-winter-spec.ts
@@ -18,6 +18,7 @@ import { withInputValidation } from "./middleware/with-input-validation.js"
 import { withUnhandledExceptionHandling } from "./middleware/with-unhandled-exception-handling.js"
 import { ResponseValidationError } from "./middleware/http-exceptions.js"
 
+console.log('test log')
 const attachMetadataToRouteFn = <
   const GS extends GlobalSpec,
   const RS extends RouteSpec<InferRecordKey<GS["authMiddleware"]>>,
