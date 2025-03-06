0f2d48665e8cf40a6d24803c752a1bb0e4ec6a5f
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-03-06 12:09:27 +0530

    Delete tests/middleware/with-route-spec.ts

--- tests/middleware/with-route-spec.ts
+++
@@ -1,9 +0,0 @@
-import { createWithWinterSpec } from "../src/create-with-winter-spec.js"
-import { withReturnTypeCheck } from "../src/middleware/with-return-type-check.js"
-
-export const withRouteSpec = createWithWinterSpec({
-  beforeAuthMiddleware: [withReturnTypeCheck],
-  authMiddleware: {},
-  apiName: "Example",
-  productionServerUrl: "https://example.com",
-})
